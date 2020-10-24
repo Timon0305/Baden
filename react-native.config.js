f2cc628779573fdecb9e559fde2d4a497a6f9a9c
Timon0305 <timon0305@outlook.com>
2020-10-24 13:42:53 +0800

    revert

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   dependencies: {
-    '@react-native-community/datetimepicker': {
+    '@react-native-community/datetimepciker': {
       platforms: {
         // android: null, // assign null to disable autolinking // remove to link
         ios: null
